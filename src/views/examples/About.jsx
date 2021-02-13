4ec9edba080d9a62ae80b25a085f971714f373fd
Celso Vanderlei <celsovanderlei@gmail.com>
2021-02-13 17:30:10 -0300

    Initial

---
+++ src/views/examples/About.jsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+const About = props => (
+    <aside className="About">
+        <h1>Sobre</h1>
+        <p>Sistema criado para teste do React.</p>
+
+    </aside>
+)
+
+export default About
