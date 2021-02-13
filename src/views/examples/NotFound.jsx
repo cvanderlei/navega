89c0a46b646f72d989d687435d245c87b33872af
Celso Vanderlei <celsovanderlei@gmail.com>
2021-02-13 18:35:53 -0300

    Final

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NotFound = props => (
+    <aside className="NotFound">
+        <h1>404</h1>
+        <p>Página não encontrada</p>
+
+    </aside>
+)
+
+export default NotFound
